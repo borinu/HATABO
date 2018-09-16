73b45ff5bd8ffd0c21894969031e4140bb313dae
ハタボリアヤナ <a-hatabori@leihauoli.com>
2018-09-16 20:02:03 +0900

    carouselフォルダ作成

--- WORKS/dist/carousel/js/jquery.carousel.js
+++ WORKS/dist/carousel/js/jquery.carousel.js
@@ -14,14 +14,14 @@
 			imageCountPerView : 1,
 			destroy : false
 		}, options);
-		
+
 		this.$frame = $frame;
 		this.init();
 	};
 	CarouselController.prototype = {
 		INDICATOR_CURRENT_CLASS : 'current',
 		TRIGGER_DISABLE_CLASS : 'disabled',
-		
+
 		init : function(){
 			this.setParameters();
 			this.createIndicators();
@@ -40,18 +40,18 @@
 			this.viewWidth = this.$wrapper.width();
 			this.viewCount = Math.ceil(this.$container.children().length / this.settings.imageCountPerView);
 			this.viewOffset = this.$container.children().length % this.settings.imageCountPerView;
-			
+
 			if(this.viewOffset == 0){
 				this.viewOffset = this.settings.imageCountPerView;
 			}
 			this.currentIndex = this.settings.slideLoop ? 1 : 0;
 			this.timerIds = [];
 			this.startAxis = null;
-			
+
 			this.$cassettes = this.$container.children();
-			
+
 			var imageWidth = this.viewWidth / this.settings.imageCountPerView;
-			
+
 			this.$cassettes.width(imageWidth);
 			this.$container.width(imageWidth * this.$cassettes.length);
 			this.carouselId = (new Date()).getTime();
@@ -63,7 +63,7 @@
 			if(this.settings.indicatorAdjustment){
 				var $indicatorTemplate = this.$indicatorContainer.children().detach(),
 					fragment = document.createDocumentFragment();
-				
+
 				for(var i = 0, count = this.viewCount; i < count; i++){
 					fragment.appendChild($indicatorTemplate.clone().get(0));
 				}
@@ -77,34 +77,34 @@
 				return;
 			}
 			var $lis = this.$container.children();
-			
+
 			for(var i = 0, length = this.settings.imageCountPerView; i < length; i++){
 				this.$container.prepend($lis.eq($lis.length - 1 - i).clone().data('clone', true));
 				this.$container.append($lis.eq(i).clone().data('clone', true));
 			}
 			this.$cassettes = this.$container.children();
-			
+
 			var imageWidth = this.viewWidth / this.settings.imageCountPerView;
-			
+
 			this.$cassettes.width(imageWidth);
 			this.$container.width(imageWidth * this.$cassettes.length);
-			
+
 			this.scrollImages(this.viewWidth, 1, 0, 0);
 		},
 		bindEvent : function(){
 			var myself = this;
-			
+
 			this.$window.on('resize.' + this.carouselId, $.proxy(this.adjustForResize, this));
 			this.$previous.on('click.' + this.carouselId, $.proxy(this.slideToPrevious, this));
 			this.$next.on('click.' + this.carouselId, $.proxy(this.slideToNext, this, false));
-			
+
 			if(this.$indicatorContainer.length > 0){
 				this.$indicators.each(function(index){
 					$(this).on('click.' + myself.carouselId, $.proxy(myself.slideByIndicator, myself, index));
 				});
 			}
 			this.$container.on('transitionend.' + this.carouselId + ' webkitTransitionEnd.' + this.carouselId, $.proxy(this.finishToMove, this));
-			
+
 			if(this.settings.flickEnabled){
 				this.$container.on('mousedown.' + this.carouselId + ' touchstart.' + this.carouselId, $.proxy(this.startToFlick, this));
 				this.$container.on('mousemove.' + this.carouselId + ' touchmove.' + this.carouselId, $.proxy(this.flick, this));
@@ -114,18 +114,18 @@
 		},
 		adjustForResize : function(){
 			var viewWidth = this.$wrapper.width();
-			
+
 			if(this.viewWidth == viewWidth){
 				return;
 			}
 			this.clearTimer();
 			this.viewWidth = viewWidth;
-			
+
 			var imageWidth = this.viewWidth / this.settings.imageCountPerView;
-			
+
 			this.$cassettes.width(imageWidth);
 			this.$container.width(imageWidth * this.$cassettes.length);
-			
+
 			this.scrollImages(this.viewWidth, this.currentIndex, 0, 0);
 			this.setTimer();
 		},
@@ -141,10 +141,10 @@
 				return;
 			}
 			event.preventDefault();
-			
+
 			var axis = this.getAxis(event),
 				dx = axis.x - this.startAxis.x;
-				
+
 			this.scrollImages(this.viewWidth, this.currentIndex,  -dx, 0);
 		},
 		endToFlick : function(event){
@@ -153,7 +153,7 @@
 			}
 			var axis = this.getAxis(event),
 				dx = axis.x - this.startAxis.x;
-				
+
 			if(dx > -this.settings.flickThreshold && dx < this.settings.flickThreshold){
 				this.scrollImages(this.viewWidth, this.currentIndex, 0, this.settings.slideTime);
 			}else{
@@ -183,7 +183,7 @@
 				event.preventDefault();
 			}
 			this.clearTimer();
-			
+
 			if(this.settings.slideLoop){
 				this.currentIndex = Math.min(this.currentIndex + 1, this.viewCount + 1);
 			}else{
@@ -195,14 +195,14 @@
 		},
 		slideByIndicator : function(index, event){
 			event.preventDefault();
-			
+
 			this.clearTimer();
 			this.currentIndex = index + (this.settings.slideLoop ? 1 : 0);
 			this.scrollImages(this.viewWidth, this.currentIndex, 0, this.settings.slideTime);
 		},
 		scrollImages : function(viewWidth, index, offset, duration){
 			var distance;
-			
+
 			if(index >= this.viewCount + (this.settings.slideLoop ? 0 : - 1)){
 				distance = viewWidth * (index - 1) + viewWidth / this.settings.imageCountPerView * this.viewOffset + offset;
 			}else{
@@ -223,7 +223,7 @@
 				this.controlTrigger();
 			}
 			this.setTimer();
-			
+
 			if(this.$indicatorContainer.length > 0){
 				this.$indicators.removeClass(this.INDICATOR_CURRENT_CLASS);
 				this.$indicators.eq(this.currentIndex + (this.settings.slideLoop ? -1 : 0)).addClass(this.INDICATOR_CURRENT_CLASS);
@@ -258,20 +258,20 @@
 		},
 		destroy : function(){
 			this.clearTimer();
-			
+
 			var myself = this;
-			
+
 			this.$window.off('resize.' + this.carouselId, this.adjustForResize);
 			this.$previous.off('click.' + this.carouselId, this.slideToPrevious);
 			this.$next.off('click.' + this.carouselId, this.slideToNext);
-			
+
 			if(this.$indicatorContainer.length > 0){
 				this.$indicators.each(function(index){
 					$(this).off('click.' + myself.carouselId, myself.slideByIndicator);
 				});
 			}
 			this.$container.off('transitionend.' + this.carouselId + ' webkitTransitionEnd.' + this.carouselId, this.finishToMove);
-			
+
 			if(this.settings.flickEnabled){
 				this.$container.on('mousedown.' + this.carouselId + ' touchstart.' + this.carouselId, this.startToFlick);
 				this.$container.on('mousemove.' + this.carouselId + ' touchmove.' + this.carouselId, this.flick);
@@ -292,7 +292,7 @@
 	$.fn.carousel = function(options){
 		if(options.destroy){
 			var controller = $(this).data('carousel-controller');
-			
+
 			if(controller){
 				controller.destroy();
 			}
